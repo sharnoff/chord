1a16312a4951d65ab1f0b1be163735ed8d4e8aee
sharnoff <git@sharnoff.io>
2023-03-07 20:03:27 -0800

    doc: fix links, typos, remove extras

--- doc.go
+++ doc.go
@@ -14,16 +14,16 @@ For an example of using all of it together, see: <https://github.com/neondatabas
 # Signal handling
 
 The general idea behind signal handling via [SignalManager] is that signals are mostly user-defined,
-trigger exactly once, and are hierarchical (i.e. triggering a signal in a child [SignalManager] does
+trigger exactly once, and are hierarchical (i.e. triggering a signal in a child SignalManager does
 not affect the parent).
 
-Users of [SignalManager] register callbacks via [SignalManager.On]. Callbacks are called at most
+Users of SignalManager register callbacks via [SignalManager.On]. Callbacks are called at most
 once, when the signal is triggered. Callbacks are processed sequentially in the reverse order of
 when they're registered, and may return error. Unhandled errors will prevent calling any remaining
 callbacks.
 
 Signals can be manually triggered with [SignalManager.Trigger]. There's special handling around
-[os.Signal] values so that forwarding into the [SignalManager] is automatically set up when
+[os.Signal] values so that forwarding into the SignalManager is automatically set up when
 required.
 
 For more, see [SignalManager].
@@ -35,7 +35,7 @@ goroutines. To that end, [GetStackTrace] may be given a parent [StackTrace] to u
 appended on producing a string.
 
 The stack trace management is designed with simplicity in mind, with optimizations for collection
-but not printing (i.e. [GetStackTrace] should be fast, but there's faster ways to print than via
+but not printing (i.e. GetStackTrace should be fast, but there's faster ways to print than via
 [StackTrace.String]).
 
 For more, see [StackTrace].
@@ -44,10 +44,9 @@ For more, see [StackTrace].
 
 [TaskGroup] is essentially a glorified [sync.WaitGroup]. The main features it adds are: subgroups,
 named Add/Done, and fetching all tasks not yet Done. It also exposes [TaskGroup.Wait] as a channel,
-so you can select over it. [TaskGroup.TryWait] takes a [Context] and does that selection for you.
+so you can select over it. [TaskGroup.TryWait] takes a [context.Context] and does that selection for
+you.
 
-For mkore, see [TaskGroup].
-
-[Context]: context.Context
+For more, see [TaskGroup].
 */
 package chord
